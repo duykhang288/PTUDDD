7a67539fd09a813ce4e8e1ec6f55722e8d9698fd
duykhang28 <101155523+duykhang288@users.noreply.github.com>
2022-10-27 14:19:44 +0700

    Cai dat chung thuc voi Firebase Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'package:myshop/ui/cart/cart_manager.dart';
 import 'package:myshop/ui/orders/order_manager.dart';
 import 'package:myshop/ui/screens.dart';
@@ -9,8 +11,10 @@ import 'ui/products/product_overview_screen.dart';
 import 'ui/products/user_products_screen.dart';
 import 'ui/cart/cart_screen.dart';
 import 'ui/orders/orders_screen.dart';
+import 'ui/screens.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load();
   runApp(const MyApp());
 }
 
@@ -21,59 +25,70 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (ctx) => ProductsManager(),
-        ),
-        ChangeNotifierProvider(
-          create: (ctx) => CartManager(),
-        ),
-        ChangeNotifierProvider(
-          create: (ctx) => OrdersManager(),
-        ),
-      ],
-      child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            fontFamily: 'Lato',
-            colorScheme: ColorScheme.fromSwatch(
-              primarySwatch: Colors.purple,
-            ).copyWith(
-              secondary: Colors.deepOrange,
-            ),
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => AuthManager(),
           ),
-          home: const ProductsOverviewScreen(),
-          routes: {
-            CartScreen.routeName: (ctx) => const CartScreen(),
-            OrderScreen.routeName: (ctx) => const OrderScreen(),
-            UserProductsScreen.routeName: (ctx) => const UserProductsScreen(),
+          ChangeNotifierProvider(
+            create: (ctx) => ProductsManager(),
+          ),
+          ChangeNotifierProvider(
+            create: (ctx) => CartManager(),
+          ),
+          ChangeNotifierProvider(
+            create: (ctx) => OrdersManager(),
+          ),
+        ],
+        child: Consumer<AuthManager>(
+          builder: (ctx, authManager, child) {
+            return MaterialApp(
+                debugShowCheckedModeBanner: false,
+                theme: ThemeData(
+                  fontFamily: 'Lato',
+                  colorScheme: ColorScheme.fromSwatch(
+                    primarySwatch: Colors.purple,
+                  ).copyWith(
+                    secondary: Colors.deepOrange,
+                  ),
+                ),
+                home: authManager.isAuth
+                    ? const ProductsOverviewScreen()
+                    : FutureBuilder(
+                        future: authManager.tryAutoLogin(),
+                        builder: (ctx, snapshot) {
+                          return snapshot.connectionState ==
+                                  ConnectionState.waiting
+                              ? const SplashScreen()
+                              : const AuthScreen();
+                        },
+                      ),
+                onGenerateRoute: (settings) {
+                  if (settings.name == ProductDetailScreen.routeName) {
+                    final productId = settings.arguments as String;
+                    return MaterialPageRoute(
+                      builder: (ctx) {
+                        return ProductDetailScreen(
+                          ctx.read<ProductsManager>().findById(productId),
+                        );
+                      },
+                    );
+                  }
+                  if (settings.name == EditProductScreen.routeName) {
+                    final productId = settings.arguments as String?;
+                    return MaterialPageRoute(
+                      builder: (ctx) {
+                        return EditProductScreen(
+                          productId != null
+                              ? ctx.read<ProductsManager>().findById(productId)
+                              : null,
+                        );
+                      },
+                    );
+                  }
+                  return null;
+                });
           },
-          onGenerateRoute: (settings) {
-            if (settings.name == ProductDetailScreen.routeName) {
-              final productId = settings.arguments as String;
-              return MaterialPageRoute(
-                builder: (ctx) {
-                  return ProductDetailScreen(
-                    ctx.read<ProductsManager>().findById(productId),
-                  );
-                },
-              );
-            }
-            if (settings.name == EditProductScreen.routeName) {
-              final productId = settings.arguments as String?;
-              return MaterialPageRoute(
-                builder: (ctx) {
-                  return EditProductScreen(
-                    productId != null
-                        ? ctx.read<ProductsManager>().findById(productId)
-                        : null,
-                  );
-                },
-              );
-            }
-            return null;
-          }),
-    );
+        ));
   }
 }
 
--- lib/ui/shared/app_drawer.dart
+++ lib/ui/shared/app_drawer.dart
@@ -1,9 +1,10 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 import '../orders/orders_screen.dart';
 import '../products/user_products_screen.dart';
+import '../auth/auth_manager.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({super.key});
@@ -42,6 +43,17 @@ class AppDrawer extends StatelessWidget {
                   .pushReplacementNamed(UserProductsScreen.routeName);
             },
           ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Logout'),
+            onTap: () {
+              Navigator.of(context)
+                ..pop()
+                ..pushReplacementNamed('/');
+              context.read<AuthManager>().logout();
+            },
+          )
         ],
       ),
     );
